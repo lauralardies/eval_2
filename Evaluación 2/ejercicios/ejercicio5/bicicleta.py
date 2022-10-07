8db4a84373852d4a9b24a2786ea9a355e4d6fa22
lauralardies <lrodrlar@myuax.com>
2022-10-07 16:30:42 +0200

    bicicleta archivo

---
+++ Evaluación 2/ejercicios/ejercicio5/bicicleta.py
@@ -0,0 +1,10 @@
+from vehiculo import Vehiculo
+
+class Bicicleta():
+
+    def __init__(self, color, ruedas, tipo):
+        Vehiculo.__init__(self, color, ruedas)
+        self.tipo = tipo
+
+    def __str__(self):
+        return Vehiculo.__str__(self) + ", {}".format(self.tipo)
