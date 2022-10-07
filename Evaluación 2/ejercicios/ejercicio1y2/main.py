ef95b705b67656f492b39c24fb30cc97ceead709
lauralardies <lrodrlar@myuax.com>
2022-10-07 17:13:51 +0200

    cambios organizacion

--- Evaluación 2/ejercicios/ejercicio1y2/main.py
+++ Evaluación 2/ejercicios/ejercicio1y2/main.py
@@ -1,4 +1,5 @@
 from alumnos import Alumnos
+from alumnospt2 import Alumnos
 
 alumno1 = Alumnos()
 alumno2 = Alumnos()
@@ -7,3 +8,7 @@ alumno3 = Alumnos()
 alumno1.calificacion()
 alumno2.calificacion()
 alumno3.calificacion()
+
+alumno1.__str__()
+alumno2.__str__()
+alumno3.__str__()
--- Evaluación 2/ejercicios/ejercicio2/main2.py
+++
@@ -1,9 +0,0 @@
-from alumnospt2 import Alumnos
-
-alumno1 = Alumnos()
-alumno2 = Alumnos()
-alumno3 = Alumnos()
-
-alumno1.__str__()
-alumno2.__str__()
-alumno3.__str__()
