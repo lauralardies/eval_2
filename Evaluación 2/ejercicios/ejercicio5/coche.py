864df0905011de7f3ae090ca57b1e5da06670f52
lauralardies <lrodrlar@myuax.com>
2022-10-07 16:36:25 +0200

    archivo motocicleta

--- Evaluación 2/ejercicios/ejercicio5/coche.py
+++ Evaluación 2/ejercicios/ejercicio5/coche.py
@@ -3,9 +3,9 @@ from vehiculo import Vehiculo
 class Coche(Vehiculo):
 
     def __init__(self, color, ruedas, velocidad, cilindrada):
-        Vehiculo.__init__(self, color, ruedas)
+        super().__init__(self, color, ruedas)
         self.velocidad = velocidad
         self.cilindrada = cilindrada
- 
+
     def __str__(self):
-        return Vehiculo.__str__(self) + ", {} km/h, {}cc".format(self.velocidad, self.cilindrada)
+        return super().__str__(self) + ", {} km/h, {}cc".format(self.velocidad, self.cilindrada)
