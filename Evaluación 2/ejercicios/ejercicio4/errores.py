db97962f44b9d51b377df2d33e64346cd5180909
lauralardies <lrodrlar@myuax.com>
2022-10-07 17:24:28 +0200

    cambios

--- Evaluación 2/ejercicios/ejercicio4/errores.py
+++ Evaluación 2/ejercicios/ejercicio4/errores.py
@@ -1,8 +1,15 @@
-error = 7/0
-def error(error):
+
+
+error ="2" + 10
+
+def error_funct(error):
     try:
         error
     except ZeroDivisionError:
         print("No se puede dividir entre 0")
     except IndexError:
         print("Puede ser que el índice indicado no exista en al lista.")
+    except KeyError:
+        print("La llave marcada no existe en el diccionario.")
+    except TypeError:
+        print("Estás sumando un str")
