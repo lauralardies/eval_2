988e5c3ddda08247d04d2cc15364212b6e7bb1c9
lauralardies <lrodrlar@myuax.com>
2022-10-07 16:14:39 +0200

    arreglo código ej 3

--- Evaluación 2/ejercicios/ejercicio3/producto.py
+++ Evaluación 2/ejercicios/ejercicio3/producto.py
@@ -1,6 +1,3 @@
-from curses.ascii import isalpha
-
-
 class Producto():
 
     def __init__(self):
@@ -8,13 +5,18 @@ class Producto():
         dato2 = input("Introduzca el nombre del producto: ")
         dato3 = input("Introduzca el precio del producto: ")
         dato4 = input("Introduzca el tipo de producto: ")
-        if dato1.isalpha() and dato2.isalpha() and dato4.isalpha():
-            if dato3.replace(",", "", 1).isalpha():
-                self.codigo = dato1
+        if dato2.isalpha() and dato4.isalpha():
+            if dato3.replace(",", "", 1).isdigit() and dato1.isdigit():
+                self.codigo = int(dato1)
                 self.nombre = dato2
                 self.precio = float(dato3)
                 self.tipo = dato4
                 print("El producto se ha creado con éxito.")
+        else:
+            print("Introdzca datos válidos.")
     
     def __str__(self):
         return print(self.codigo, self.nombre, self.precio, self.tipo)
+
+producto = Producto()
+producto.__str__()
