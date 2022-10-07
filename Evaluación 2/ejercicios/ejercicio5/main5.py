86594595f403397d9121aa897a4c2cad60f685bf
lauralardies <lrodrlar@myuax.com>
2022-10-07 17:12:24 +0200

    cambios

--- Evaluación 2/ejercicios/ejercicio5/main5.py
+++ Evaluación 2/ejercicios/ejercicio5/main5.py
@@ -5,12 +5,11 @@ from motocicleta import Motocicleta
 from vehiculo import Vehiculo
 
 c = Coche("azul", 4, 150, 1200)
-c1 = Coche("azul", 4, 150, 1200)
 ca = Camioneta("blanca", 4, 120, 1200, 6000)
 b = Bicicleta("amarilla", 2, "deportiva")
 m = Motocicleta("verde", 2, "urbana", 170, 1200)
 
-vehiculos = [c, c1, ca, b, m]
+vehiculos = [c, ca, b, m]
 v = Vehiculo
 v.catalogar(vehiculos, 0)
 v.catalogar(vehiculos, 2)
