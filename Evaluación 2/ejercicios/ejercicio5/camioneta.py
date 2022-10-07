7beb406c801e53644bceb4201c2fd7eb33b1f682
lauralardies <lrodrlar@myuax.com>
2022-10-07 16:28:00 +0200

    clase camioneta

---
+++ Evaluación 2/ejercicios/ejercicio5/camioneta.py
@@ -0,0 +1,10 @@
+from coche import Coche
+
+class Camioneta(Coche):
+
+    def __init__(self, color, ruedas, velocidad, cilindrada, carga):
+        super().__init__(color, ruedas, velocidad, cilindrada)
+        self.carga = carga
+    
+    def __str__(self):
+        return super().__str__() + ", {} kg".format(self.carga)
