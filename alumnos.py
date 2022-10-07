4338ffaecc678b9d104a090818e6285b511e2ea3
lauralardies <lrodrlar@myuax.com>
2022-10-07 15:51:40 +0200

    pruebas superadas y códugo mejorado

--- alumnos.py
+++ alumnos.py
@@ -1,15 +1,30 @@
-from mailbox import NoSuchMailboxError
-
-
 class Alumnos():
 
-    def __init__(self, nombre, nota) -> None:
-        self.nombre = nombre
-        self.nota = nota
-        print("El alumno se ha creado con éxito.")
-    
+    def __init__(self):
+
+        dato1 = input("Introduzca el nombre del alumno: ")
+        dato2 = input("Introduzca la nota del alumno: ")
+        if dato1.isalpha():
+            if dato2.isdigit():
+                dato2 = int(dato2)
+                if 0<=dato2<=10:
+                    self.nombre = dato1
+                    self.nota = dato2
+                    print("El alumno se ha creado con éxito.")
+        
+        else:
+            print("Debe introducir datos válidos.")
+            
     def calificacion(self):
         if self.nota >= 5:
             print("El alumno ha aprobado.")
         else:
             print("El alumno ha suspendido")
+
+alumno1 = Alumnos()
+alumno2 = Alumnos()
+alumno3 = Alumnos()
+
+alumno1.calificacion()
+alumno2.calificacion()
+alumno3.calificacion()
